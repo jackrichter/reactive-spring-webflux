104142ae91ba5eacc56940a2e261ae7d9d387d18
jackrichter <jackrichter1@gmail.com>
2024-08-18 11:22:09 +0200

    Movie Info Service - initial commit - corrections.

--- movie-info-service/src/main/java/com/reactivespring/MoviesInfoServiceApplication.java
+++ movie-info-service/src/main/java/com/reactivespring/MoviesInfoServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MovieInfoServiceApplication {
+public class MoviesInfoServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MovieInfoServiceApplication.class, args);
+		SpringApplication.run(MoviesInfoServiceApplication.class, args);
 	}
 
 }
